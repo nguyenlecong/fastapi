59f5a8b2867a720ecee1e77891d0885ebb736925
Le Cong Nguyen <62042231+nguyenlecong@users.noreply.github.com>
2025-06-23 09:14:03 +0700

    Update discord_bot.py

--- build_api/app/discord_bot.py
+++ build_api/app/discord_bot.py
@@ -3,7 +3,7 @@ import requests
 
 class DiscordBot():
     def __init__(self):
-        self.url = "https://discord.com/api/webhooks/1379312413987569714/5ZPASJcN_gWSV692D5BuTvvWnZGZi0EJkDYz91yl4DtrXRnSqoAx8ZXvJ_lrMHX3cqzW"
+        self.url = ""
         self.headers = {"Authorization": ""}
     
     def send_message(self, text):
